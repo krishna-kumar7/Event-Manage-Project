644a5fa903a5eca781a61fad8491954eb1d5b337
Ashutosh <ashutosh18052004@gmail.com>
2025-03-26 22:19:32 +0530

    PartiesSection

--- Event-Frontend/src/App.jsx
+++ Event-Frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import EventSection from "./components/EventSection";
 import WeddingSec from "./components/WeddingSec";
-
+import PartiesSec from "./components/PartiesSec";
 
 const App = () => {
   return (
     <div>
      
-     <WeddingSec />
+     <PartiesSec />
     </div>
   );
 };
